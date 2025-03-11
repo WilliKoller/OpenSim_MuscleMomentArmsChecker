e961f7208f40754c7da382df366b79fc4a92ac48
Willi Koller <willi.koller@gmx.at>
2025-03-11 16:20:50 +0100

    Update compareMuscleParams.m

--- compareMuscleParams.m
+++ compareMuscleParams.m
@@ -2,10 +2,12 @@ import org.opensim.modeling.*;
 
 clear;
 
-kinematicFolder = './ExampleData/kinematics';
+kinematicFolder = 'C:\Users\willi\ucloud\PhD\Study_LongitudinalMSK\SimulationOutput_Sanguex_GRF20Hz\TD06_S2_adjCond_modWO_opt_N10_final\gait03\Output\IK';
 kinematicFileFilter = '*.mot';
-modelFilename = './ExampleData/model.osim';
+modelFilename = 'C:\Users\willi\ucloud\PhD\Study_LongitudinalMSK\Models_Sangeux\TD06_S2_generic_final.osim';
 modelFilenameModified = strrep(modelFilename, '.osim', '_modWO.osim');
+modelFilenameModified = 'C:\Users\willi\ucloud\PhD\Study_LongitudinalMSK\Models_Sangeux\OptimizedModels/TD06_S2_adjCond_modWO_opt_N10_final.osim';
+% modelFilename = '';
 
 motFiles = dir(fullfile(kinematicFolder, '**', kinematicFileFilter));
 motionFileNames = cell(1, numel(motFiles));
@@ -15,17 +17,25 @@ end
 filterFrequency = 6;
 
 % for rajagopal or gait2392 model
-coordinateNames = {'hip_flexion_l', 'hip_rotation_l', 'hip_adduction_l', ...
-    'hip_flexion_r', 'hip_rotation_r', 'hip_adduction_r', ...
-    'knee_angle_l', 'knee_angle_r'};
-
-% % for lernagopal
 % coordinateNames = {'hip_flexion_l', 'hip_rotation_l', 'hip_adduction_l', ...
 %     'hip_flexion_r', 'hip_rotation_r', 'hip_adduction_r', ...
-%     'knee_angle_l', 'knee_angle_r', 'knee_rotation_l', 'knee_rotation_r', 'knee_adduction_l', 'knee_adduction_r'};
+%     'knee_angle_l', 'knee_angle_r'};
+% 
+% % % for lernagopal
+% coordinateNames = {'hip_flexion_l', 'hip_rotation_l', 'hip_adduction_l', ...
+%     'hip_flexion_r', 'hip_rotation_r', 'hip_adduction_r', ...
+%     'knee_angle_l', 'knee_angle_r', 'knee_adduction_l', 'knee_adduction_r', ...
+%     'ankle_angle_l', 'ankle_angle_r', 'subtalar_angle_l', 'subtalar_angle_r'};
+% % for lernagopal
+coordinateNames = {'hip_flexion_r', 'hip_rotation_r', 'hip_adduction_r', ...
+    'knee_angle_r', 'knee_adduction_r', 'ankle_angle_r', 'subtalar_angle_r'};
 
 % specifiy muscle and coordinates that you want to plot
-muscleFilter = {'glmax1_l'};
+muscleFilter = {'_r'};
+
+% muscleFilter = {'add', 'gl', 'semi', 'bf', 'pec', 'grac', 'piri', 'sar', ...
+%     'tfl', 'iliacus', 'psoas', 'rect', 'gas', 'quad_fem', 'gem', 'peri', 'vas'};
+
 plotCoordinates = coordinateNames(1:3);
 
 % set factor to -1 if you want to reverse direction in plot
@@ -37,10 +47,13 @@ plotCoordinatesNames{2} = 'hip external rotation_l';
 plotCoordinatesNames{3} = 'hip abduction_l';
 
 plotMomentArms = 1;
+plotRMSDThreshold = 3e-3;
 plotMuscleLength = 1;
-tileLayout = [1, 3];
-figureSize = [0 0.05 0.5 0.2]; % normalized to windows size
-figureSizeMuscleLength = [0 0.05 0.5/3 0.2]; % normalized to windows size
+plotRMSDLengthThreshold = 3e-3;
+rowCount = ceil(sqrt(numel(plotCoordinatesNames)));
+tileLayout = [rowCount, ceil(numel(plotCoordinatesNames) / rowCount)];
+figureSize = [0 0.05 0.9 0.9]; % normalized to windows size
+figureSizeMuscleLength = [0 0.05 0.6 0.6]; % normalized to windows size
 verbose = 1;
 threshold = 0.004;
 
@@ -58,11 +71,13 @@ stateModified = modelModified.initSystem();
 
 coordInd = zeros(1, numel(coordinateNames));
 coordinateHandles = cell(1, numel(coordinateNames));
+coordIndMod = zeros(1, numel(coordinateNames));
 coordinateHandlesModified = cell(1, numel(coordinateNames));
 for i = 1 : numel(coordinateNames)
     coordInd(i) = model.getCoordinateSet().getIndex(coordinateNames{i});
     coordinateHandles{i} = model.updCoordinateSet().get(coordInd(i));
-    coordinateHandlesModified{i} = modelModified.updCoordinateSet().get(coordInd(i));
+    coordIndMod(i) = modelModified.getCoordinateSet().getIndex(coordinateNames{i});
+    coordinateHandlesModified{i} = modelModified.updCoordinateSet().get(coordIndMod(i));
 end
 
 numMuscles = model.getMuscles().getSize();
@@ -73,15 +88,19 @@ for i = 0 : numMuscles - 1
     tmp_muscleName = char(model.getMuscles().get(i).getName());
     % find muscles that fulfill filter requirements
     if contains(tmp_muscleName, muscleFilter)
-        muscleIndices = [muscleIndices, i];
-        muscleNames{end+1} = tmp_muscleName;
-        muscleHandles{end+1} = model.getMuscles().get(i);
-        muscleHandlesModified{end+1} = modelModified.getMuscles().get(i);
+        % check if muscle with this id is the same in the modified model
+        tmp_muscleNameMod = char(modelModified.getMuscles().get(i).getName());
+        if strcmp(tmp_muscleName, tmp_muscleNameMod)
+            muscleIndices = [muscleIndices, i];
+            muscleNames{end+1} = tmp_muscleName;
+            muscleHandles{end+1} = model.getMuscles().get(i);
+            muscleHandlesModified{end+1} = modelModified.getMuscles().get(i);
+        else
+            disp(['Muscle IDs of ' tmp_muscleName ' are not the same, probably the models are modified extensively.']);
+            return;
+        end
     end
 end
-
-coordSet = model.updCoordinateSet();
-coordSetModified = modelModified.updCoordinateSet();
 
 momentArmsAreWrong = 0;
 
@@ -97,12 +116,19 @@ for u = 1 : numel(motionFileNames)
         motion.lowpassIIR(filterFrequency)
     end
 
+    motionCoordinates = motion.getColumnLabels();
+    motionCoordinateNames = [];
+    for c = 0 : motionCoordinates.getSize - 1
+        motionCoordinateNames{c+1} = char(motionCoordinates.get(c));
+    end
+
     momentArmsCurrMotion = zeros(motion.getSize(), length(muscleIndices), numel(coordinateNames));
     muscleLengthCurrMotion = zeros(motion.getSize(), length(muscleIndices));
     for frame = 1:motion.getSize()
         % set all coordinates to values of the motion
         for i = 1 : numel(coordinateNames)
-            tmpAngle = motion.getStateVector(frame-1).getData().get(coordInd(i));
+            motionIdx = find(ismember(motionCoordinateNames, coordinateNames{i}) == 1, 1);
+            tmpAngle = motion.getStateVector(frame-1).getData().get(motionIdx - 2);
             if motion.isInDegrees
                 tmpAngle = tmpAngle / 180 * pi;
             end
@@ -123,12 +149,14 @@ for u = 1 : numel(motionFileNames)
             muscleLengthCurrMotion(frame, m) = muscleHandles{m}.getLength(state);
         end
     end
+
     momentArmsCurrMotionModified = zeros(motion.getSize(), length(muscleIndices), numel(coordinateNames));
     muscleLengthCurrMotionModified = zeros(motion.getSize(), length(muscleIndices));
     for frame = 1:motion.getSize()
         % set all coordinates to values of the motion
         for i = 1 : numel(coordinateNames)
-            tmpAngle = motion.getStateVector(frame-1).getData().get(coordInd(i));
+            motionIdx = find(ismember(motionCoordinateNames, coordinateNames{i}) == 1, 1);
+            tmpAngle = motion.getStateVector(frame-1).getData().get(motionIdx - 2);
             if motion.isInDegrees
                 tmpAngle = tmpAngle / 180 * pi;
             end
@@ -151,42 +179,15 @@ for u = 1 : numel(motionFileNames)
     end
     toc
 
-    discontinuitiesCurrMotion = [];
-    % check for discontinuities
-    for i = 1 : numel(coordinateNames)
-        for m = 1 : numel(muscleHandles)
-            dy = diff(momentArmsCurrMotion(:, m, i));
-            discontinuity_indices = find(abs(dy) > threshold);
-            if size(discontinuity_indices, 1) > 0
-                for d = 1 : size(discontinuity_indices, 1)
-                    discontinuitiesCurrMotion(end+1, :) = [discontinuity_indices(d), m , i];
-                end
-            end
-        end
-    end
-
-    if size(discontinuitiesCurrMotion, 1) > 0
-        if verbose
-            fprintf(2, ['Following discontinuities were detected in file \n\t' strrep(motionFileNames{u}, '\', '/') '\n']);
-            for d = 1 : size(discontinuitiesCurrMotion, 1)
-                fprintf(2,  [muscleNames{discontinuitiesCurrMotion(d, 2)} ' around ' coordinateNames{discontinuitiesCurrMotion(d, 3)} ' at ' num2str(motion.getStateVector(discontinuitiesCurrMotion(d, 1)).getTime) ' seconds (frame ' num2str(discontinuitiesCurrMotion(d, 1)) ')\n']);
-            end
-        else
-            musclesWithDiscont = unique(discontinuitiesCurrMotion(:, 2));
-            tmpText = 'Discontinuities detected for ';
-            for m = 1 : numel(musclesWithDiscont)
-                tmpText = [tmpText muscleNames{musclesWithDiscont(m)} ' '];
-            end
-            disp(tmpText);
-        end
-        momentArmsAreWrong = 1;
-    end
-
     %%
+
+    numberOfMuscles = size(momentArmsCurrMotionModified, 2);
+    colors = parula(numberOfMuscles);
 
     if plotMomentArms
         figure('Units','normalized', 'Position',figureSize);
         tiledlayout(tileLayout(1), tileLayout(2), 'TileSpacing','tight', 'Padding','tight');
+        sgtitle(['Visualization of muscle moment arms with a RMSD between models > ' num2str(plotRMSDThreshold)]);
         for i = 1 : numel(plotCoordinates)
             idxCoordinateHandle = find(contains(coordinateNames, plotCoordinates(i)));
 
@@ -194,10 +195,15 @@ for u = 1 : numel(motionFileNames)
             ylabel([strrep(plotCoordinatesNames{i}(1:end-2), '_', ' ') ' moment arm [cm]'], 'Interpreter', 'none');
             xlabel('% gait cycle');
             hold on;
+            legendArr = [];
             for m = 1 : numel(muscleHandles)
-                if abs(nansum(momentArmsCurrMotion(:, m, idxCoordinateHandle))) > 1e-5
-                    plot(0:100, normalizetimebase(momentArmsCurrMotion(:, m, idxCoordinateHandle)) * 100 * coordinateFactors(i), 'color', [0.8500 0.3250 0.0980], 'LineWidth', 2);
-                    plot(0:100, normalizetimebase(momentArmsCurrMotionModified(:, m, idxCoordinateHandle)) * 100 * coordinateFactors(i), 'color', [0 0.4470 0.7410], 'LineWidth', 2);
+                rmsd = rmse(momentArmsCurrMotion(:, m, idxCoordinateHandle), momentArmsCurrMotionModified(:, m, idxCoordinateHandle));
+                if abs(nansum(momentArmsCurrMotion(:, m, idxCoordinateHandle))) > 1e-5 && rmsd > plotRMSDThreshold
+                    disp([plotCoordinates{i} ': ' muscleNames{m} ' RMSD = ' num2str(rmsd)]);
+                    plot(0:100, normalizetimebase(momentArmsCurrMotion(:, m, idxCoordinateHandle)) * 100 * coordinateFactors(i), '-', 'color', colors(m, :), 'LineWidth', 1);
+                    plot(0:100, normalizetimebase(momentArmsCurrMotionModified(:, m, idxCoordinateHandle)) * 100 * coordinateFactors(i), '--', 'color', colors(m, :), 'LineWidth', 2);
+                    legendArr{end+1} = muscleNames{m};
+                    legendArr{end+1} = [muscleNames{m} ' modified'];
                 end
             end
             xlim([0 100]);
@@ -210,30 +216,38 @@ for u = 1 : numel(motionFileNames)
             end
             ylim(ylims);
 
-            if i == 1
-                legend({'original model', 'modified model'}, 'Interpreter', 'none', 'Location', 'best');
-            end
+            legend(legendArr, 'Interpreter', 'none', 'location', 'best');
+
         end
         drawnow;
     end
     
+    %%
     if plotMuscleLength
         figure('Units','normalized', 'Position',figureSizeMuscleLength);
-        tiledlayout(1, numel(muscleNames), 'TileSpacing','tight', 'Padding','tight');
+        % tiledlayout(1, numel(muscleNames), 'TileSpacing','tight', 'Padding','tight');
+        tiledlayout(1, 1);
+        sgtitle(['Visualization of muscle lengths with a RMSD between models > ' num2str(plotRMSDLengthThreshold)]);
+        nexttile;
 
+        legendArr = [];
+        ylabel('muscle length [cm]', 'Interpreter', 'none');
+        xlabel('% gait cycle');
+        hold on;
         for m = 1 : numel(muscleNames)
-            nexttile;
-            ylabel([muscleNames{m} ' length [cm]'], 'Interpreter', 'none');
-            xlabel('% gait cycle');
-            hold on;
+            rmsd = rmse(muscleLengthCurrMotion(:, m), muscleLengthCurrMotionModified(:, m));
+            if rmsd > plotRMSDLengthThreshold
+                plot(0:100, normalizetimebase(muscleLengthCurrMotion(:, m)) * 100, '-', 'color', colors(m, :), 'LineWidth', 1);
+                plot(0:100, normalizetimebase(muscleLengthCurrMotionModified(:, m)) * 100, '--', 'color', colors(m, :), 'LineWidth', 2);
+                legendArr{end+1} = muscleNames{m};
+                legendArr{end+1} = [muscleNames{m} ' modified'];
+            end
 
-            plot(0:100, normalizetimebase(muscleLengthCurrMotion(:, m)) * 100, 'color', [0.8500 0.3250 0.0980], 'LineWidth', 2);
-            plot(0:100, normalizetimebase(muscleLengthCurrMotionModified(:, m)) * 100, 'color', [0 0.4470 0.7410], 'LineWidth', 2);
-
-            xlim([0 100]);
-
-            legend({'original model', 'modified model'}, 'Interpreter', 'none', 'Location', 'best');
+            % legend({'original model', 'modified model'}, 'Interpreter', 'none', 'Location', 'best');
         end
+        xlim([0 100]);
+        leg = legend(legendArr, 'Interpreter', 'none', 'location', 'best');
+        leg.Layout.Tile = 'east';
         drawnow;
     end
 end
